2ecb0f3b226224fac308dc846cce376c7c864fa1
Felipe Silva <felipequefez@gmail.com>
2020-10-20 22:33:17 -0300

    state implement

--- templates/state_template/@name_state.dart
+++ templates/state_template/@name_state.dart
@@ -2,16 +2,46 @@ part of '@name_bloc.dart';
 
 class @NameState extends DefaultState {
 
-  @NameState();
+  @NameState({
+    bool isLoading,
+    bool isEmpty,
+    bool isError,
+    String errorMessage,    
+  }) : super(
+          isLoading: isLoading,
+          isEmpty: isEmpty,
+          isError: isError,
+          errorMessage: errorMessage,
+        );
 
   factory @NameState.initial() {
-    return @NameState();
+    return @NameState(
+      isEmpty: false,
+      isError: false,
+      isLoading: false,
+      errorMessage: null,
+    );
   }
 
-  @NameState copyWith() {
-    return @NameState();
+  @NameState copyWith({
+    bool isLoading,
+    bool isEmpty,
+    bool isError,
+    String errorMessage,
+  }) {
+    return @NameState(
+      isLoading: isLoading ?? this.isLoading,
+      isEmpty: isEmpty ?? this.isEmpty,
+      isError: isError ?? this.isError,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
   }
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [
+    isLoading,
+    isEmpty,
+    isError,
+    errorMessage,
+  ];
 }
