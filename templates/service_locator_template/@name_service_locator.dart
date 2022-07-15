588f200731091c7e4a75f3eda4bb9aa7da3304bf
FelipeQueFez <felipequefez@gmail.com>
2022-07-15 11:43:28 -0300

    service locator

---
+++ templates/service_locator_template/@name_service_locator.dart
@@ -0,0 +1,42 @@
+import 'package:@project/src/features/@name/data/datasources/@name_remote_data_source.dart';
+import 'package:@project/src/features/@name/data/repositories/@name_repository_impl.dart';
+import 'package:@project/src/features/@name/domain/repositories/@name_repository.dart';
+import 'package:@project/src/features/@name/domain/usecases/@name_use_case.dart';
+import 'package:@project/src/features/@name/presentation/stores/@name_store.dart';
+
+class @NameServiceLocatorConfig
+    implements ServiceLocatorConfigInternals, ServiceLocatorConfig {
+  @override
+  Future<void> configure(ServiceLocatorWrapper sl) async {
+    registerManagerState(sl);
+    registerUseCases(sl);
+    registerRepositories(sl);
+    registerDataSources(sl);
+    registerServices(sl);
+  }
+
+  @override
+  void registerManagerState(ServiceLocatorWrapper sl) {
+    sl.registerFactory(() => @NameStore());
+  }
+
+  @override
+  void registerDataSources(ServiceLocatorWrapper sl) {
+    sl.registerLazySingleton<@NameRemoteDataSource>(
+        () => @NameRemoteDataSourceImpl());
+  }
+
+  @override
+  void registerRepositories(ServiceLocatorWrapper sl) {
+    sl.registerLazySingleton<@NameRepository>(() =>
+        @NameRepositoryImpl(remoteDataSource: sl<@NameRemoteDataSource>()));
+  }
+
+  @override
+  void registerUseCases(ServiceLocatorWrapper sl) {
+    sl.registerFactory(() => @NameUseCase(sl<@NameRepository>()));
+  }
+
+  @override
+  void registerServices(ServiceLocatorWrapper sl) {}
+}
